bf4546caac8d9b38e4cca1d1fbbeac30ddfa1fce
Ben Tapfuma <takutapfu@gmail.com>
2019-08-13 06:50:49 +0200

    editing signup.js

--- controllers/signup.js
+++ controllers/signup.js
@@ -11,7 +11,7 @@ const handleSignup = (req, res, db, bcrypt)  => {//dependences injection
         return res.status(400).json('incorrect form submission');
     }
     const hash = bcrypt.hashSync(password,saltRounds);
-    console.log(hash);
+    console.log('here1');
     db.transaction(trx => //create a transact when you want to do more than 2 things at once
         {
             trx.insert({
@@ -21,6 +21,7 @@ const handleSignup = (req, res, db, bcrypt)  => {//dependences injection
             .into('login')
             .returning('email')
             .then(loginEmail => {
+                console.log('here2');
                 return trx('users')
                 .returning('*')
                 .insert({
@@ -30,12 +31,14 @@ const handleSignup = (req, res, db, bcrypt)  => {//dependences injection
                 })
                 .then(user => {
                     res.json(user[0])
+                    console.log('here3');
             })
         })
         .then(trx.commit)// for the entries to be added into the users tables from login
         .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('Unable to register'))
+    console.log('here4');
    };
    
    module.exports = {
